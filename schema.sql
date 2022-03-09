cdf49788e2b1bdd98784296cd9fb42a9f4cdc3b1
Mike_Jackson <mikejackson35@gmail.com>
2022-03-08 21:32:59 -0800

    commit1

---
+++ schema.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE golf_eat (
+	state TEXT,
+	address VARCHAR(255),
+	categories VARCHAR(255),
+	restaurant_city TEXT,
+	restaurant TEXT,
+	disc_golf_course VARCHAR(255),
+	disc_golf_city TEXT
+);
